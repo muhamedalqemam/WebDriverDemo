3fa276a240a0212aba32395137cde39e0deb7d26
muhamedalqemam <muhamed.alqemam@gmail.com>
2024-04-09 00:07:25 +0200

    v1.01

--- src/WebDriver/ChromeDemo.java
+++ src/WebDriver/ChromeDemo.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ChromeDemo {
-
+ 
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "D:\\progs\\testing\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
 		
